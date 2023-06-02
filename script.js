fbe666a15dbd929f0c2132275d215ec04b2cd5d0
Chauhan-yuvraj <113570060+Chauhan-yuvraj@users.noreply.github.com>
2023-06-02 19:54:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,24 @@ const navigation = document.querySelector('nav ul');
 menuToggle.addEventListener('click', () => {
   navigation.classList.toggle('show');
 });
+
+
+
+const icons = document.querySelectorAll('.skill-icons span');
+
+icons.forEach((icon) => {
+  icon.addEventListener('mouseover', () => {
+    icon.classList.add('hovered');
+  });
+
+  icon.addEventListener('mouseout', () => {
+    icon.classList.remove('hovered');
+  });
+});
+
+
+window.addEventListener('load', function() {
+  var loader = document.getElementById('loading-screen');
+  loader.classList.add('loaded');
+});
+
